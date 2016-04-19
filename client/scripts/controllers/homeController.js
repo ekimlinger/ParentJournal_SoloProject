4faf3773ea438841e4f37ba1efc625fff94e3e32
ekimlinger <ekimlinger@gmail.com>
2016-04-19 16:44:25 -0500

    Removed gruntfile and client folder

--- client/scripts/controllers/homeController.js
+++
@@ -1,5 +0,0 @@
-myApp.controller("HomeController", ["$scope", "JournalService", function($scope, JournalService){
-    $scope.currentUser = JournalService.currentUser;
-    $scope.randomPhrase = JournalService.randomPhrase;
-    $scope.randomJournal = JournalService.randomJournal;
-}]);
