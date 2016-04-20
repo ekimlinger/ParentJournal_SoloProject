1c54a47ff0e66bcd6696e21828378300f397c291
ekimlinger <ekimlinger@gmail.com>
2016-04-19 21:02:14 -0500

    Resending to heroku after updating mLabs

--- server/app.js
+++ server/app.js
@@ -36,6 +36,7 @@ app.use(session({
     cookie: {maxAge: 6000000000000000000000000000, secure: false}
 }));
 
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
