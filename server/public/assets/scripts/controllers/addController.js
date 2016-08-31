17d64c5a96180bac95aa83c2163684e5b9cd345c
ekimlinger <ekimlinger@gmail.com>
2016-08-30 22:15:06 -0500

    Defaulted journal entry to first child in list and added dialog for if the user has no childrenon record

--- server/public/assets/scripts/controllers/addController.js
+++ server/public/assets/scripts/controllers/addController.js
@@ -1,10 +1,48 @@
-myApp.controller("AddController", ["$scope", "$filter", "$mdToast", "JournalService", function($scope, $filter, $mdToast, JournalService){
+myApp.controller("AddController", ["$scope", "$filter", "$mdToast", "$mdDialog", "$mdMedia", "JournalService", function($scope, $filter, $mdToast, $mdDialog, $mdMedia, JournalService){
+
+  //Function in case user has not entered child info
+  $scope.addChild = function() {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+    $mdDialog.show({
+        controller: DialogController,
+        templateUrl: '../../assets/views/partials/addChildOnJournal.tmpl.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose: false,
+        fullscreen: useFullScreen
+      })
+      .then(function(child) {
+        $scope.entry.child = JournalService.currentUser.data.children[0].firstname;
+        $scope.entry.phrases = [];
+        $scope.entry.things = [];
+        $scope.entry.rating = 3;
+        $scope.entry.accomplishments = [];
+      });
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+  };
+
   // Declares/ clears entry form
+  console.log(JournalService.currentUser.data);
   $scope.user = JournalService.currentUser.data;
   $scope.entry = {};
   $scope.entry.phrases = [];
   $scope.entry.things = [];
+  $scope.entry.rating = 3;
   $scope.entry.accomplishments = [];
+
+  $scope.$watch("user.children", function(user){
+    // If the user has children entered default to first child
+    if(JournalService.currentUser.data.children[0]){
+      $scope.entry.child = JournalService.currentUser.data.children[0].firstname;
+
+    } else{     // Show dialogue
+      $scope.addChild();
+    }
+  });
+
 
   //Sets default date to today
   $scope.entry.date = new Date($filter("date")(Date.now()));
@@ -15,11 +53,11 @@ myApp.controller("AddController", ["$scope", "$filter", "$mdToast", "JournalServ
       $mdToast.simple()
       .textContent('Created New Entry')
       .position('fixed')
-      .hideDelay(7000)
+      .hideDelay(3000)
     );
   };
 
-  //Sends entry form to factory
+  // Sends entry form to factory
   $scope.submit = function(entry){
     JournalService.postEntries(entry);
     $scope.showToast(entry.date);
@@ -29,4 +67,28 @@ myApp.controller("AddController", ["$scope", "$filter", "$mdToast", "JournalServ
     $scope.entry.accomplishments = [];
   };
 
+
+
+
+
+  //All dialog controls
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.addChild = function(child) {
+      if(child && child.dob && child.firstname && child.lastname){
+        JournalService.addChild(child);
+        $mdDialog.hide(child);
+      } else{
+        $scope.changeStatus = "Please enter all info!"
+      }
+    };
+
+  }
 }]);
